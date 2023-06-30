6b7677de245d3310bcedfa0863165b86e758c12b
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-30 13:22:22 +0530

    chore: Create post entity and configure db

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,15 @@
-console.log("Hello, world!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const post_1 = require("./entities/post");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [post_1.Post],
+        dbName: "lireddit",
+        type: "postgresql",
+        debug: !constants_1.__prod__,
+    });
+};
+main();
 //# sourceMappingURL=index.js.map
